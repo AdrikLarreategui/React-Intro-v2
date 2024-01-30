6243fdda7a95904efb8c92db9f09bca5a9882a0b
AdrikLarreategui <adrian15186@hotmail.com>
2024-01-30 17:40:56 +0100

    commit

---
+++ React Intro v2/Ej 2/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>My page</h1>
+                <nav>
+                    <ul>
+                        <li><a href ="home"></a></li>
+                    </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
---
+++ React Intro v2/Ej 2/src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div id = "home">
+                <h2>Welcome to my homepage</h2>
+                <p>Hi, I'm Adrián and I'm learning React! </p>
+            </div>
+        )
+    }
+};
+
+
