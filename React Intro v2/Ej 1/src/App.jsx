bbddfa142b254508f2dc48b13b05dc2eb784b337
AdrikLarreategui <adrian15186@hotmail.com>
2024-01-30 17:40:32 +0100

    commit Ejercicio 2 empezado

--- React Intro v2/Ej 1/src/App.jsx
+++ React Intro v2/Ej 1/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Person } from './components/Person'
+import PersonClass from './components/PersonClass'
 
 function App() {
   const [count, setCount] = useState(0)
